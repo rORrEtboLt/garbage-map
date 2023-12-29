fca5ccde34ccaef9c9512c0464c0a7b68fff6730
Nesara Windows <nesara.hippla@gmail.com>
2023-12-29 18:13:56 +0530

    GM0002 - Made few changes to auth check added userid and user, Garbage map addition API complete.

--- express/server/models/garbage_map/garbage_map.js
+++ express/server/models/garbage_map/garbage_map.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
+const Joi = require('joi');
 const Schema = mongoose.Schema;
 
 // Garbage map model schema
 const definition = {
-  reporter: Object,
-  userId: Schema.Types.ObjectId,
+  userId: Schema.Types.ObjectId, // the one who reports isnt it?
   latitude: Number,
   longitude: Number,
-  pollutionType: String,
-  info: String,
-  imageUrl: Array,
+  pollutionType: String, // Type of pollution can be [ AIR, WATER, GARBAGE, ORGANIC, PLASTIC, MEDICAL, RECYLABLE, SOUND] 
+  info: String, // Description
+  imageUrl: Array, // array of image urls
   createdAt: {
     type: Date,
     default: Date.now(),
   },
-  updatedAT: {
+  updatedAt: {
     type: Date,
     default: Date.now(),
   },
@@ -30,4 +30,11 @@ const garbageMapsSchema = new Schema(definition, options);
 // Garbage map model creation
 const garbageMapModel = mongoose.model("garbageMaps", garbageMapsSchema);
 
-module.exports = { garbageMapModel, garbageMapsSchema };
+const garbageMapValidationSchema = Joi.object().keys({
+  userId: Joi.string().required(),
+  latitude: Joi.number().required(),
+  longitude: Joi.number().required(),
+  pollutionType: Joi.string().required(),
+});
+
+module.exports = { garbageMapModel, garbageMapsSchema, garbageMapValidationSchema };
--- express/server/routers/api_router.js
+++ express/server/routers/api_router.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = new express.Router();
 const {
-  listGabageMapForLatLon
+  listGabageMapForLatLon,
+  addGabageMapForLatLon
 } = require("../controllers/garbage_map_controller");
 
-// /garbage-map/:lat/:log Adding account personal data into the system
-router.post("/garbage-map/:lat/:log", (req, res) => {
+// /garbage-map/:lat/:log Adding garbage map data into the system
+router.get("/garbage-map/:lat/:log", (req, res) => {
   listGabageMapForLatLon(req, res);
+});
+
+// /garbage-map/:lat/:log Adding garbage map data into the system
+router.post("/garbage-map", (req, res) => {
+  addGabageMapForLatLon(req, res);
 });
 
 module.exports = router;
